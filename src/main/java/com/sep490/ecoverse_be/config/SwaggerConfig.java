374de780a218d47957e0dff75af6ca43c952ff15
unknown <HuynhTuanPhat-git>
2026-03-12 03:06:11 +0700

    fix: issue

--- src/main/java/com/sep490/ecoverse_be/config/SwaggerConfig.java
+++ src/main/java/com/sep490/ecoverse_be/config/SwaggerConfig.java
@@ -26,7 +26,10 @@ public class SwaggerConfig {
                         .description("Local Development Server"))
                 .addServersItem(new Server()
                         .url("http://13.212.15.242:8080")
-                        .description("Production Server"))
+                        .description("HTTP Production Server"))
+                .addServersItem(new Server()
+                        .url("https://13.212.15.242:8080")
+                        .description("HTTPS Production Server"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEMES))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEMES,
